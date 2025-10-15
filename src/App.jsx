503b96775318f4bb77896965507221f8d4397f7d
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-15 18:56:38 +0300

    Update project files

---
+++ src/App.jsx
@@ -0,0 +1,282 @@
+export default function App() {
+  return (
+    <div className="home blog custom-background ">
+      <header id="branding">
+	<div className="container clearfix">
+		<div className="hgroup-wrap clearfix">
+			<section className="hgroup-right">
+				<div className="social-profiles clearfix">
+					<ul>
+					</ul>
+				</div>
+				<form action="https://zpep.com.ua/" className="searchform clearfix" method="get">
+					<label className="assistive-text" htmlFor="ss">Поиск</label>
+					<input type="text" className="s field" name="s" id="ss" style={{ width: '65px' }} />
+				</form>
+				<div>
+					<a href="https://zpep.com.ua/call_centr/">
+						<div style={{ paddingTop: '10px', color: '#05a9c5', marginRight: '140px', width: '250px' }}><b>Кол-центр:</b></div>
+						<div className="phone-numbers">
+							<div className="tel1">061 228 22 20</div>
+							<div className="tel1">066 228 22 20</div>
+							<div className="tel1">063 228 22 20</div>
+							<div className="tel1">068 228 22 20</div>
+						</div>
+					</a>
+				</div>
+				<button className="menu-toggle">Responsive Menu</button>
+			</section>
+			<hgroup id="site-logo" className="clearfix">
+				<h1 id="site-title">
+					<a href="https://zpep.com.ua/" title="ТОВ &quot;Запоріжжяелектропостачання&quot;" rel="home">
+						<img src="https://zpep.com.ua/wp-content/uploads/2020/04/logo_zpep_m.png" alt="ТОВ &quot;Запоріжжяелектропостачання&quot;" />
+					</a>
+				</h1>
+			</hgroup>
+		</div>
+	</div>
+	<div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+		<div className="carousel-inner">
+			<div className="carousel-item active">
+				<a href="https://zpep.com.ua/"><img className="d-block w-100" src="https://zpep.com.ua/wp-content/uploads/2024/12/banner4c.jpg" alt="First slide" /></a>
+			</div>
+		</div>
+	</div>
+	<nav id="access" className="clearfix">
+		<div className="container clearfix">
+			<ul className="root">
+				<li id="menu-item-122" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-122"><a href="https://zpep.com.ua/spozhivacham/">Електроенергія</a>
+					<ul className="sub-menu">
+						<li id="menu-item-8394" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-8394"><a href="https://zpep.com.ua/rozdiltarifs/">Ціни та тарифи</a></li>
+						<li id="menu-item-358" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-358"><a href="https://zpep.com.ua/vilni-tsiny/">Постачання за вільними цінами</a></li>
+						<li id="menu-item-1315" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1315"><a href="https://zpep.com.ua/vilni-tsiny-byudzhetni-ustanovy/">Вільні ціни бюджетні установи</a></li>
+						<li id="menu-item-367" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-367"><a href="https://zpep.com.ua/mali-nepobutovi-spozhivachi/">Малі непобутові споживачі</a></li>
+						<li id="menu-item-465" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-465"><a href="https://zpep.com.ua/budzhetni-ustanovy/">Бюджетні установи</a></li>
+						<li id="menu-item-357" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-357"><a href="https://zpep.com.ua/pobutovi-spozhivachi/">Побутові споживачі</a></li>
+					</ul>
+				</li>
+				<li id="menu-item-9003" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-9003"><a href="https://zpep.com.ua/prirodniy-gaz/">Природний газ</a></li>
+				<li id="menu-item-28" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-28"><a href="https://zpep.com.ua/normatyvno-pravovi-akty/">Нормативно-правові акти</a></li>
+				<li id="menu-item-356" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-356"><a href="https://zpep.com.ua/documenty-ta-informatsiya/">Документи та інформація</a>
+					<ul className="sub-menu">
+						<li id="menu-item-486" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-486"><a href="https://zpep.com.ua/publichni-zakupivli/">Закупівлі</a></li>
+						<li id="menu-item-1834" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1834"><a href="https://zpep.com.ua/zvitnist/">Звітність</a></li>
+					</ul>
+				</li>
+				<li id="menu-item-5556" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-5556"><a href="https://zpep.com.ua/faq-2/">Поширені запитання</a></li>
+				<li id="menu-item-6228" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-6228"><a href="https://zpep.com.ua/contacts/">Контакти</a></li>
+				<li id="menu-item-10025" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-10025"><a href="https://zpep.com.ua/sitemap/">Мапа сайту</a></li>
+			</ul>
+		</div>
+	</nav>
+	<section className="featured-slider">
+		<div className="slider-cycle"></div>
+		<nav id="controllers" className="clearfix">
+		</nav>
+	</section>
+</header>
+
+
+      <section className="hgroup-right">
+    <div className="social-profiles clearfix">
+        <ul>
+        </ul>
+    </div>
+    <form action="https://zpep.com.ua/" className="searchform clearfix" method="get">
+        <label className="assistive-text" htmlFor="ss">Поиск</label>
+        <input type="text" className="s field" name="s" id="ss" style={{ width: '65px' }} />
+    </form>
+    <div>
+        <a href="https://zpep.com.ua/call_centr/">
+            <div style={{ paddingTop: '10px', color: '#05a9c5', marginRight: '140px', width: '250px' }}><b>Кол-центр:</b></div>
+            <div className="phone-numbers">
+                <div className="tel1">061 228 22 20</div>
+                <div className="tel1">066 228 22 20</div>
+                <div className="tel1">063 228 22 20</div>
+                <div className="tel1">068 228 22 20</div>
+            </div>
+        </a>
+    </div>
+    <button className="menu-toggle">Responsive Menu</button>
+</section>
+
+
+      <section className="featured-slider">
+  <div className="slider-cycle"></div>
+  <nav id="controllers" className="clearfix">
+  </nav>
+</section>
+
+
+      <section id="post-10450" className="post-10450 post type-post status-publish format-standard has-post-thumbnail hentry category-news">
+	<article>
+		<header className="entry-header">
+			<h2 className="entry-title">
+				<a href="https://zpep.com.ua/2025/10/08/chas-splatyty-rakhunky-za-elektroenerhiiu-za-veresen/" title="ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ">ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ</a>
+			</h2>
+		</header>
+		<figure className="post-featured-image">
+			<a href="https://zpep.com.ua/2025/10/08/chas-splatyty-rakhunky-za-elektroenerhiiu-za-veresen/" title="ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ">
+				<img width="670" height="300" src="https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень-670x300.jpeg" className="attachment-featured size-featured wp-post-image" alt="ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ" title="ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ" decoding="async" fetchpriority="high" srcSet="https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень-670x300.jpeg 670w, https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень-300x134.jpeg 300w, https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень-1024x459.jpeg 1024w, https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень-768x344.jpeg 768w, https://zpep.com.ua/wp-content/uploads/2025/10/Час-сплатити-жовтень.jpeg 1340w" sizes="(max-width: 670px) 100vw, 670px" />
+			</a>
+		</figure>
+		<div className="entry-content clearfix">
+			<p>Шановні споживачі електричної енергії! ТОВ «Запоріжжяелектропостачання» інформує, що рахунки за спожиту електричну енергію у вересні 2025 року направлені до банків та фінансових установ. Сплатити за електроенергію Ви можете за допомогою: 1. Платіжних систем: 2. Інтернет-банкінгу за реквізитами: 3. Терміналів самообслуговування.&hellip; </p>
+		</div>
+		<div className="entry-meta-bar clearfix">	        			
+			<div className="entry-meta">
+				<span className="date updated">
+					<a href="https://zpep.com.ua/2025/10/08/chas-splatyty-rakhunky-za-elektroenerhiiu-za-veresen/" title="06:46">08.10.2025</a>
+				</span> |
+				<span className="category">
+					<a href="https://zpep.com.ua/category/news/" rel="category tag">Новини</a>
+				</span> 
+			</div>
+			<a className="readmore" href="https://zpep.com.ua/2025/10/08/chas-splatyty-rakhunky-za-elektroenerhiiu-za-veresen/" title="ЧАС СПЛАТИТИ РАХУНКИ ЗА ЕЛЕКТРОЕНЕРГІЮ">Читати далі</a>
+		</div>
+	</article>
+</section>
+
+
+      <section id="post-10403" className="post-10403 post type-post status-publish format-standard has-post-thumbnail hentry category-6">
+	<article>
+		<header className="entry-header">
+			<h2 className="entry-title">
+				<a href="https://zpep.com.ua/2025/10/04/z-dnem-mista-2/" title="З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!">З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!</a>
+			</h2>
+		</header>
+		<figure className="post-featured-image">
+			<a href="https://zpep.com.ua/2025/10/04/z-dnem-mista-2/" title="З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!">
+				<img width="670" height="300" src="https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста-670x300.jpeg" className="attachment-featured size-featured wp-post-image" alt="З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!" title="З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!" decoding="async" srcSet="https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста-670x300.jpeg 670w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста-300x134.jpeg 300w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста-1024x459.jpeg 1024w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста-768x344.jpeg 768w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-міста.jpeg 1340w" sizes="(max-width: 670px) 100vw, 670px" />
+			</a>
+		</figure>
+		<div className="entry-content clearfix">
+			<p>Запоріжжя −&nbsp;це&nbsp;місто, яке стало символом мужності, єдності та нескореного духу України. Серце козацької слави, промисловий гігант,&nbsp;місто сили та співчуття. Але передусім −&nbsp;це люди, які щодня творять історію, захищають&nbsp;рідну землю, підтримують&nbsp;одне одного і не втрачають&nbsp;віри у перемогу.Нехай у кожному домі буде&hellip; </p>
+		</div>
+		<div className="entry-meta-bar clearfix">	        			
+			<div className="entry-meta">
+				<span className="date updated"><a href="https://zpep.com.ua/2025/10/04/z-dnem-mista-2/" title="06:20">04.10.2025</a></span> |
+				<span className="category"><a href="https://zpep.com.ua/category/%d0%b7%d1%96-%d1%81%d0%b2%d1%8f%d1%82%d0%be%d0%bc/" rel="category tag">Зі святом</a></span>
+			</div>
+			<a className="readmore" href="https://zpep.com.ua/2025/10/04/z-dnem-mista-2/" title="З ДНЕМ МІСТА, НЕЗЛАМНЕ ЗАПОРІЖЖЯ!">Читати далі</a>
+		</div>
+	</article>
+</section>
+
+
+      <section id="post-10378" className="post-10378 post type-post status-publish format-standard has-post-thumbnail hentry category-news">
+	<article>
+		<header className="entry-header">
+			<h2 className="entry-title">
+				<a href="https://zpep.com.ua/2025/10/02/chas-peredaty-pokazy-lichylnyka-za-veresen/" title="ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА">ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА</a>
+			</h2>
+		</header>
+		<figure className="post-featured-image">
+			<a href="https://zpep.com.ua/2025/10/02/chas-peredaty-pokazy-lichylnyka-za-veresen/" title="ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА">
+				<img width="670" height="300" src="https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень-670x300.jpeg" className="attachment-featured size-featured wp-post-image" alt="ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА" title="ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА" decoding="async" srcSet="https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень-670x300.jpeg 670w, https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень-300x134.jpeg 300w, https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень-1024x459.jpeg 1024w, https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень-768x344.jpeg 768w, https://zpep.com.ua/wp-content/uploads/2025/09/Час-передати-вересень.jpeg 1340w" sizes="(max-width: 670px) 100vw, 670px" />
+			</a>
+		</figure>
+		<div className="entry-content clearfix">
+			<p>Шановні споживачі! Розпочинається період передачі показів приладу обліку електроенергії за вересень: Кодексом комерційного обліку електричної енергії, затвердженим постановою НКРЕКП від 14.03.2018 № 311, п.8.6.3. передбачено термін передачі показів приладу обліку електроенергії 5 днів — останні 2 дні поточного місяця та 3&hellip; </p>
+		</div>
+		<div className="entry-meta-bar clearfix">
+			<div className="entry-meta">
+				<span className="date updated">
+					<a href="https://zpep.com.ua/2025/10/02/chas-peredaty-pokazy-lichylnyka-za-veresen/" title="07:47">02.10.2025</a>
+				</span> |
+				<span className="category">
+					<a href="https://zpep.com.ua/category/news/" rel="category tag">Новини</a>
+				</span>
+			</div>
+			<a className="readmore" href="https://zpep.com.ua/2025/10/02/chas-peredaty-pokazy-lichylnyka-za-veresen/" title="ЧАС ПЕРЕДАТИ ПОКАЗИ ЕЛЕКТРОЛІЧИЛЬНИКА">Читати далі</a>
+		</div>
+	</article>
+</section>
+
+
+      <section id="post-10388" className="post-10388 post type-post status-publish format-standard has-post-thumbnail hentry category-6">
+	<article>
+		<header className="entry-header">
+			<h2 className="entry-title">
+				<a href="https://zpep.com.ua/2025/10/01/z-dnem-zakhysnykiv-i-zakhysnyts-ukrainy/" title="З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!">З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!</a>
+			</h2>
+		</header>
+		<figure className="post-featured-image">
+			<a href="https://zpep.com.ua/2025/10/01/z-dnem-zakhysnykiv-i-zakhysnyts-ukrainy/" title="З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!">
+				<img width="670" height="300" src="https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України-670x300.jpeg" className="attachment-featured size-featured wp-post-image" alt="З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!" title="З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!" decoding="async" loading="lazy" srcSet="https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України-670x300.jpeg 670w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України-300x134.jpeg 300w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України-1024x459.jpeg 1024w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України-768x344.jpeg 768w, https://zpep.com.ua/wp-content/uploads/2025/10/З-Днем-захисників-і-захисниць-України.jpeg 1340w" sizes="(max-width: 670px) 100vw, 670px" />
+			</a>
+		</figure>
+		<div className="entry-content clearfix">
+			<p>Сьогодні серце кожного українця б’ється в унісон із тими, хто тримає небо над нами. З тими,  хто щодня стоїть на сторожі нашої свободи, гідності та майбутнього. Ви − наші герої, наш щит і наша гордість.Дякуємо за кожен день, коли ми можемо жити,&hellip; </p>
+		</div>
+		<div className="entry-meta-bar clearfix">
+			<div className="entry-meta">
+				<span className="date updated"><a href="https://zpep.com.ua/2025/10/01/z-dnem-zakhysnykiv-i-zakhysnyts-ukrainy/" title="06:36">01.10.2025</a></span> |
+				<span className="category"><a href="https://zpep.com.ua/category/%d0%b7%d1%96-%d1%81%d0%b2%d1%8f%d1%82%d0%be%d0%bc/" rel="category tag">Зі святом</a></span>
+			</div>
+			<a className="readmore" href="https://zpep.com.ua/2025/10/01/z-dnem-zakhysnykiv-i-zakhysnyts-ukrainy/" title="З ДНЕМ ЗАХИСНИКІВ І ЗАХИСНИЦЬ УКРАЇНИ!">Читати далі</a>
+		</div>
+	</article>
+</section>
+
+
+      <section id="post-10336" className="post-10336 post type-post status-publish format-standard has-post-thumbnail hentry category-news">
+	<article>
+		<header className="entry-header">
+			<h2 className="entry-title">
+				<a href="https://zpep.com.ua/2025/09/22/mobilnyi-dodatok-zep-vidteper-i-dlia-ios/" title="МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS">МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS</a>
+			</h2>
+		</header>
+		<figure className="post-featured-image">
+			<a href="https://zpep.com.ua/2025/09/22/mobilnyi-dodatok-zep-vidteper-i-dlia-ios/" title="МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS">
+				<img width="670" height="300" src="https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS-670x300.jpeg" className="attachment-featured size-featured wp-post-image" alt="МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS" title="МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS" decoding="async" loading="lazy" srcSet="https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS-670x300.jpeg 670w, https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS-300x134.jpeg 300w, https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS-1024x459.jpeg 1024w, https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS-768x344.jpeg 768w, https://zpep.com.ua/wp-content/uploads/2025/09/Моб.-додаток-IOS.jpeg 1340w" sizes="(max-width: 670px) 100vw, 670px" />
+			</a>
+		</figure>
+		<div className="entry-content clearfix">
+			<p>ТОВ «Запоріжжяелектропостачання» −&nbsp;компанія, яка прагне розуміти та задовольняти потреби клієнтів, створюючи для них зручні сервіси. Ми запускаємо мобільний додаток&nbsp;«ЗЕП» для операційної системи IOS.&nbsp;Тепер «Запоріжжяелектропостачання» у Вашому смартфоні, з наступними основними функціями: Користуватися мобільним додатком можна в будь-яку годину доби та з&hellip; </p>
+		</div>
+		<div className="entry-meta-bar clearfix">
+			<div className="entry-meta">
+				<span className="date updated">
+					<a href="https://zpep.com.ua/2025/09/22/mobilnyi-dodatok-zep-vidteper-i-dlia-ios/" title="07:32">22.09.2025</a>
+				</span> |
+				<span className="category">
+					<a href="https://zpep.com.ua/category/news/" rel="category tag">Новини</a>
+				</span>
+			</div>
+			<a className="readmore" href="https://zpep.com.ua/2025/09/22/mobilnyi-dodatok-zep-vidteper-i-dlia-ios/" title="МОБІЛЬНИЙ ДОДАТОК «ЗЕП» ВІДТЕПЕР І ДЛЯ IOS">Читати далі</a>
+		</div>
+	</article>
+</section>
+
+
+      <footer id="colophon" className="clearfix">
+  <div id="site-generator" className="clearfix">
+    <div className="container">
+      <div className="copyright">&copy;2025 <a href="https://zpep.com.ua/" title="ТОВ &quot;Запоріжжяелектропостачання&quot;"><span>ТОВ &quot;Запоріжжяелектропостачання&quot;</span></a></div>
+    </div>
+  </div>
+  <div className="back-to-top"><a href="#branding"></a></div>
+  
+  <div className="social-buttons">
+    <a href="https://apps.apple.com/ua/app/%D0%B7%D0%B5%D0%BF/id6739285275?l=uk" target="_blank" rel="noopener noreferrer">
+      <img src="https://zpep.com.ua/wp-content/uploads/2025/01/App-Store_-e1757405454880.png" alt="ЗЕП IOS" />
+    </a>
+
+    <a href="https://play.google.com/store/apps/details?id=com.zep.stage&hl=uk" target="_blank" rel="noopener noreferrer">
+      <img src="https://zpep.com.ua/wp-content/uploads/2025/01/google-play-badge-crop.png" alt="zpep app" />
+    </a>
+
+    <a href="https://www.facebook.com/zpep.com.ua" target="_blank" rel="noopener noreferrer">
+      <img src="https://zpep.com.ua/wp-content/uploads/2025/01/fb.png" alt="Facebook" />
+    </a>
+
+    <a href="https://www.instagram.com/zp_ep_/" target="_blank" rel="noopener noreferrer">
+      <img src="https://zpep.com.ua/wp-content/uploads/2025/01/inst.png" alt="Instagram" />
+    </a>
+  </div>
+</footer>
+
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-1': '#8212',
+        'custom-2': '#fff',
+        'custom-3': '#32373c',
+        'custom-4': '#000000',
+        'custom-5': '#abb8c3',
+        'custom-6': '#ffffff',
+        'custom-7': '#f78da7',
+        'custom-8': '#cf2e2e',
+        'custom-9': '#ff6900',
+        'custom-10': '#fcb900',
+        'custom-11': '#7bdcb5',
+        'custom-12': '#00d084',
+        'custom-13': '#8ed1fc',
+        'custom-14': '#0693e3',
+        'custom-15': '#9b51e0'
+      },
+      fontFamily: {
+        'custom-1': ['PT Sans%7CPhilosopher', 'sans-serif']
+      }
+    }
+  }
+};
